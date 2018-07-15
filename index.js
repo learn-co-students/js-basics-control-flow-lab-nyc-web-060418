abc7675c139a85a6f9bb9100cad7658ddf12bf75
Ms-Alex <ms.hernandeza1@gmail.com>
2018-07-15 00:52:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(distance) {
+  let result;
+
+  if (distance <= 400) {
+    result = "This one is on me!";
+  }
+  if (distance > 2000 && distance < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  }
+  else if (distance > 2500) {
+    result = "No can do.";
+  }
+
+  return result;
+}
+
+
+function ternaryCheckCity(destination) {
+  let result;
+
+  destination === "NYC"? result = "Ok, sounds good." : result = "No go."
+
+  return result;
+}
+
+function switchOnCharmFromTip(tip){
+  let result;
+
+  switch (tip) {
+    case "generous":
+      result = "Thank you so much.";
+      break;
+    case "not as generous":
+      result = "Thank you.";
+      break;
+    default:
+      result = "Bye."
+      break;
+  }
+  return result;
+}
