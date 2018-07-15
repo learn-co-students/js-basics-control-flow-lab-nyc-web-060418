00c9b380de447d46d567ca25b6f43025a96b725f
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-07-15 16:22:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+function scuberGreetingForFeet(inputInt) {
+  if (inputInt <= 400) {
+    return "This one is on me!"
+  }
+  else if (inputInt > 2000 && inputInt < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (inputInt > 2500) {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(inputDestination) {
+  let answer;
+  inputDestination === 'NYC' ? answer = 'Ok, sounds good.' : answer = 'No go.'
+  return answer
+}
+
+function switchOnCharmFromTip(inputTip) {
+  switch (inputTip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default :
+      return 'Bye.';
+  }
+}
