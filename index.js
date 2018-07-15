36613aedd05b924599b6f71c00346134a4a1c362
slowbeam <sedwardscode@gmail.com>
2018-07-15 13:02:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let response
+  if (feet <= 400) {
+    response = 'This one is on me!'
+  }
+  else if (feet > 2500) {
+    response  = 'No can do.'
+  }
+  else if (feet > 2000) {
+    response  = 'I will gladly take your thirty bucks.'
+  }
+    return response
+}
+
+function ternaryCheckCity(city) {
+  let response = city != 'NYC' ? 'No go.' : 'Ok, sounds good.'
+
+  return response
+}
+
+function switchOnCharmFromTip(tip) {
+
+  let response;
+
+  switch (tip) {
+    case 'generous':
+    response = 'Thank you so much.';
+    break;
+    case 'not as generous':
+    response = 'Thank you.'
+    break;
+    default:
+    response = 'Bye.'
+    break
+
+  }
+  return response
+}
