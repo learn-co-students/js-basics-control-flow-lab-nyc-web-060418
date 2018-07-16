ec07afc389f2658aca2a4a992ef6269d339d3736
Shing Wong <shing2487@gmail.com>
2018-07-15 23:04:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+function scuberGreetingForFeet(value) {
+  let result;
+  if (value <= 400) {
+    result = "This one is on me!";
+  } else if (value > 2000 && value < 2500) {
+    result = "I will gladly take your thirty bucks."
+  } else {
+    result = "No can do."
+  }
+
+  return result;
+};
+
+function ternaryCheckCity(city) {
+  let result = city === "NYC" ? "Ok, sounds good." : "No go.";
+
+  return result;
+}
+
+function switchOnCharmFromTip(tip) {
+  let result;
+
+  switch (tip) {
+    case 'generous':
+      result = "Thank you so much.";
+      break;
+    case 'not as generous':
+      result = "Thank you.";
+      break;
+    default:
+      result = "Bye.";
+  }
+
+  return result;
+}
